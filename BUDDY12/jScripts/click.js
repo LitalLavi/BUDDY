3526ba51152c2020369544f5cded1aa7295fea2e
LitalLavi <50523146+LitalLavi@users.noreply.github.com>
2021-08-21 18:43:53 +0300

    Update click.js

--- BUDDY12/jScripts/click.js
+++ BUDDY12/jScripts/click.js
@@ -1,8 +1,8 @@
-$('document').ready(function () {
+//$('document').ready(function () {
 
-    //check if user already logged in , if not - redirect to login page
-    if (document.cookie == "" || document.cookie == undefined) {
-        window.location.href = "login.html";
-        return;
-    }
-})
+//    //check if user already logged in , if not - redirect to login page
+//    if (document.cookie == "" || document.cookie == undefined) {
+//        window.location.href = "login.html";
+//        return;
+//    }
+//})
