74831e6c1461ea9a2e03ae583f3b9caeec89e03f
LitalLavi <50523146+LitalLavi@users.noreply.github.com>
2021-08-21 18:38:32 +0300

    Add files via upload

---
+++ BUDDY12/jScripts/all-page-content/click_exercise_data.js
@@ -0,0 +1,32 @@
+﻿export default [
+    {
+        id: "click_exercise_freeRun",
+        headline: "הרצה חופשית של הכלב",
+        allText: `ריצה חופשי שחרור כיף דשא`,
+    },
+
+    {
+    id: "click_exercise_noFoodFloor",
+    headline: "סירוב מזון/לא לאכול מהרצפה",
+    allText: `אוכל מזון סירוב לא מסכים לא רוצה רצפה `,
+  },
+
+  {
+    id: "click_exercise_commandLeave",
+    headline: "הפקודה עזוב",
+    allText: `פקודה פקודות עזוב שחרר חפצים חפץ צעצועים `,
+  },
+
+  {
+    id: "click_exercise_reactName",
+    headline: "תגובת הגור לשמו",
+    allText: `קריאה בשם תגובה שם גור גורה`,
+  },
+
+  {
+    id: "click_exercise_fistFood",
+    headline: "תרגיל אגרוף ",
+    allText: `אגרוף מוציא אוכל גרגירים אליי כפתור פאוץ' `,
+  },
+
+];
