07fbe2f599b150e4cba7e66d2a6c3cc3a0b1b5af
LitalLavi <50523146+LitalLavi@users.noreply.github.com>
2021-08-21 18:34:45 +0300

    Delete jScripts directory

--- jScripts/Info.js
+++
@@ -1,8 +0,0 @@
-$('document').ready(function () {
-
-    //check if user already logged in , if not - redirect to login page
-    if (document.cookie == "" || document.cookie == undefined) {
-        window.location.href = "login.html";
-        return;
-    }
-})
